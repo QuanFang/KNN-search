afb36c3b6743d2e56553b3ac1a3b4c4d76f0e334
zhuhan <zhuhan10@gmail.com>
2014-11-08 21:00:21 +0800

    final

--- hashfunctions.cc
+++ hashfunctions.cc
@@ -3,6 +3,8 @@
 #include <ctime>
 #include <assert.h>
 #include <iostream>
+#include <sstream>
+#include <math.h>
 
 using namespace std;
 
@@ -29,6 +31,70 @@ Projections::~Projections(){
         }
         delete [] projections_;
     }
+}
+
+PProjections::PProjections(){
+    l_ = -1;
+    k_ = -1;
+    projections_ = NULL;
+}
+
+PProjections::PProjections(int l, int k, float width){
+    assert((l >= 0) && (k >= 0));
+    l_ = l;
+    k_ = k;
+    width_ = width;
+    projections_ = new p_function[l_];
+    for(int i = 0;i < l_;++i){
+        projections_[i].vectors_ = new float*[k_];
+        projections_[i].random_b_ = new float[k_];
+        for(int j = 0;j < k_;++j){
+            projections_[i].vectors_[j] = new float[DIMENSION];
+        }
+    }
+}
+
+PProjections::~PProjections(){
+    if(projections_ != NULL){
+        for(int i = 0;i < l_;++i){
+            for(int j = 0;j < k_;++j){
+                delete [] projections_[i].vectors_[j];
+            }
+            delete [] projections_[i].random_b_;
+            delete [] projections_[i].vectors_;
+        }
+        delete [] projections_;
+    }
+}
+
+float gaussrand(float mean, float standard_deviation)
+{
+    static float V1, V2, S;
+    static int phase = 0;
+    float X;
+     
+    if(phase == 0){
+        do{
+            float U1 = (float)rand() / RAND_MAX;
+            float U2 = (float)rand() / RAND_MAX;
+             
+            V1 = 2 * U1 - 1;
+            V2 = 2 * U2 - 1;
+            S = V1 * V1 + V2 * V2;
+        } while(S >= 1 || S == 0);
+         
+        X = V1 * sqrt(-2 * log(S) / S);
+    } else
+        X = V2 * sqrt(-2 * log(S) / S);
+         
+    phase = 1 - phase;
+ 
+    return X;
+}
+
+p_function *PProjections::get_p_function(int i){
+    assert((projections_ != NULL) && (i >= 0) && (i <= l_));
+    return (projections_ + i);
 }
 
 HashFunctions::HashFunctions(){
@@ -63,6 +129,39 @@ bool HashFunctions::insert(int hash_key, MyVector *value, int index){
     return true;
 }
 
+PHashFunctions::PHashFunctions(){
+    l_ = -1;
+    k_ = -1;
+    hashmaps_ = NULL;
+}
+
+PHashFunctions::PHashFunctions(int l, int k){
+    assert((l >= 0) && (k >= 0));
+    l_ = l;
+    k_ = k;
+    hashmaps_ = new unordered_map<string, list<MyVector *> >[l_];
+}
+
+PHashFunctions::~PHashFunctions(){
+    if(hashmaps_ != NULL){
+        delete [] hashmaps_;
+    }
+}
+
+bool PHashFunctions::insert(string hash_key, MyVector *value, int index){
+    auto search_result = hashmaps_[index].find(hash_key);
+    if(search_result == hashmaps_[index].end()){ // not found
+        list<MyVector *> new_list;
+        new_list.push_back(value);
+        hashmaps_[index].insert(make_pair(hash_key, new_list));
+    }
+    else{
+        search_result->second.push_back(value);
+        //cout << search_result->second.size() << endl;
+    }
+    return true;
+}
+
 Projections *get_random_projections(int l, int k, int max_coordinate){
     srand((unsigned int)time(0));
     Projections *projections = new Projections(l, k);
@@ -73,6 +172,41 @@ Projections *get_random_projections(int l, int k, int max_coordinate){
     }
     
     return projections;
+}
+
+PProjections *get_random_p_projections(int l, int k, int width, float mean, float standard_deviation){
+    srand((unsigned int)time(0));
+    PProjections *projections = new PProjections(l, k, width);
+    for(int i = 0;i < l;++i){
+        for(int j = 0;j < k;++j){
+            projections->projections_[i].random_b_[j] = rand() % width;
+            for(int m = 0;m < DIMENSION;++m){
+                projections->projections_[i].vectors_[j][m] = gaussrand(mean, standard_deviation);
+            }
+        }
+    }
+    
+    return projections;
+}
+
+float product(float *data_array, float *p_vector, int length){
+    float result = 0;
+    for(int i = 0;i < length;++i){
+        result += data_array[i] * p_vector[i];
+    }
+    return result;
+}
+
+string gen_p_hash_key(float *data_array, p_function *projection, int p_length, float width){
+    string hash_key = "";
+    for(int i = 0;i < p_length;++i){
+        int temp = (int)((product(data_array, projection->vectors_[i], DIMENSION) + projection->random_b_[i]) / width);
+        stringstream n_str;
+        n_str << temp;
+        hash_key += "," + n_str.str();
+    }
+    cout << hash_key << endl;
+    return hash_key;
 }
 
 int gen_hash_key(float *data_array, int *projection, int p_length){
@@ -98,6 +232,35 @@ bool hash_insert(HashFunctions *hash_functions, Data *data, Projections *project
         }
     }
     return true;
+}
+
+bool p_hash_insert(PHashFunctions *hash_functions, Data *data, PProjections *projections){
+    for(int i = 0;i < hash_functions->l_;++i){
+        for(int j = 0;j < data->size_;++j){
+            string hash_key = gen_p_hash_key(data->get_vector(j)->vector_, projections->get_p_function(i), projections->k_, projections->width_);
+            hash_functions->insert(hash_key, data->get_vector(j), i);
+        }
+    }
+    return true;
+}
+
+set<MyVector *> get_p_near_vectors(PHashFunctions *hash_functions, MyVector *vector, PProjections *projections){
+    set<MyVector *> result_set = set<MyVector *>();
+    for(int i = 0;i < hash_functions->l_;++i){
+        string hash_key = gen_p_hash_key(vector->vector_, projections->get_p_function(i), projections->k_, projections->width_);
+        auto search_result = hash_functions->hashmaps_[i].find(hash_key);
+        if(search_result != hash_functions->hashmaps_[i].end()){
+            list<MyVector *> temp_list = search_result->second;
+            list<MyVector *>::iterator temp_iterator = temp_list.begin();
+            while(temp_iterator != temp_list.end()){
+                result_set.insert(*temp_iterator);
+                temp_iterator++;
+            }
+        }
+    }
+    
+    result_set.erase(vector);
+    return result_set;
 }
 
 set<MyVector *> get_near_vectors(HashFunctions *hash_functions, MyVector *vector, Projections *projections){
--- main.cc
+++ main.cc
@@ -6,19 +6,40 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-    char *filename = argv[1]; //data file path
-    int projections_num = atoi(argv[2]); //l in the paper, number of projections
-    int projection_size = atoi(argv[3]); //k in the paper, size of a projection
-    
-    Data *data = load_data_from_file(filename);  //load data
-    Projections *projections = get_random_projections(projections_num, projection_size, 1000); //randomly generate projection coordinates
-    HashFunctions *hash_functions = new HashFunctions(projections_num, projection_size); //init hash functions
+    char *mode = argv[1];
+    if(mode[0] == 'p' or mode [0] == 'P'){
+        char *filename = argv[2]; //data file path
+        int projections_num = atoi(argv[3]); //l in the paper, number of projections
+        int projection_size = atoi(argv[4]); //k in the paper, size of a projections
+        int width = atoi(argv[5]); //width of p-stable hashing
+        float d_mean = atof(argv[6]); //mean value of normal distribution
+        float d_deviation = atof(argv[7]); //standard deviation of normal distribution
+        
+        Data *data = load_data_from_file(filename);  //load data
+        PProjections *projections = get_random_p_projections(projections_num, projection_size, width, d_mean, d_deviation); //generate projection randomly
+        PHashFunctions *hash_functions = new PHashFunctions(projections_num, projection_size); //init hash functions
 
-    hash_insert(hash_functions, data, projections);//apply hash funtions to the data
-    
-    set<MyVector *> query_result = get_near_vectors(hash_functions, data->get_vector(0), projections); //example of query, returns a set of vectors
-    
-    cout << query_result.size() << endl;
+        p_hash_insert(hash_functions, data, projections);//apply hash funtions to the data
+        
+        set<MyVector *> query_result = get_p_near_vectors(hash_functions, data->get_vector(0), projections); //example of query, returns a set of vectors
+        
+        cout << query_result.size() << endl;
+    }
+    else{
+        char *filename = argv[2]; //data file path
+        int projections_num = atoi(argv[3]); //l in the paper, number of projections
+        int projection_size = atoi(argv[4]); //k in the paper, size of a projection
+        
+        Data *data = load_data_from_file(filename);  //load data
+        Projections *projections = get_random_projections(projections_num, projection_size, 1000); //randomly generate projection coordinates
+        HashFunctions *hash_functions = new HashFunctions(projections_num, projection_size); //init hash functions
+
+        hash_insert(hash_functions, data, projections);//apply hash funtions to the data
+        
+        set<MyVector *> query_result = get_near_vectors(hash_functions, data->get_vector(0), projections); //example of query, returns a set of vectors
+        
+        cout << query_result.size() << endl;   
+    }
 
     return 0;
 }
